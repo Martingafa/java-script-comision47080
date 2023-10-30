8d2bb2b7b106ac28feb7caeb588d0540575b1fdc
Martingafa <martingafa0123@gmail.com>
2023-10-30 20:49:11 -0300

    AGREGADO DE MEMORY STORAGE Y FILTRO

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 class Producto {
   constructor(nombre, valorEnPesos) {
     this.nombre = nombre;
@@ -12,21 +11,27 @@ class Producto {
   }
 }
 
-let nombreProducto = document.getElementById('nombreProducto');
-let valorPesos = document.getElementById('valorPesos');
-let agregarProductoBtn = document.getElementById('agregarProductoBtn');
-let calcularBtn = document.getElementById('calcularBtn');
-let filtroInput = document.getElementById('filtro');
-let listaProductos = document.getElementById('listaProductos');
-let productos = [];
+const nombreProducto = document.getElementById('nombreProducto');
+const valorPesos = document.getElementById('valorPesos');
+const agregarProductoBtn = document.getElementById('agregarProductoBtn');
+const filtroInput = document.getElementById('filtro');
+const listaProductos = document.getElementById('listaProductos');
+const fechaHora = document.getElementById('fechaHora');
+let productos = obtenerProductosDesdeLocalStorage() || [];
+
+agregarProductoBtn.addEventListener('click', agregarProducto);
+filtroInput.addEventListener('input', filtrarProductos);
+
+mostrarProductos();
 
 function agregarProducto() {
-  let productoNombre = nombreProducto.value;
-  let valorEnPesos = parseFloat(valorPesos.value);
+  const productoNombre = nombreProducto.value;
+  const valorEnPesos = parseFloat(valorPesos.value);
 
   if (productoNombre && !isNaN(valorEnPesos)) {
-    let nuevoProducto = new Producto(productoNombre, valorEnPesos);
+    const nuevoProducto = new Producto(productoNombre, valorEnPesos);
     productos.push(nuevoProducto);
+    guardarProductosEnLocalStorage(productos);
     mostrarProductos();
     nombreProducto.value = '';
     valorPesos.value = '';
@@ -35,32 +40,61 @@ function agregarProducto() {
   }
 }
 
-function mostrarProductos() {
+function mostrarProductos(productosMostrados) {
   listaProductos.innerHTML = '';
-  productos.forEach(function(producto) {
-    let li = document.createElement('li');
-    li.textContent = `Producto: ${producto.nombre}, Valor en pesos: ${producto.valorEnPesos}, Precio en dólares: $${producto.precioEnDolares.toFixed(2)}`;
-    listaProductos.appendChild(li);
-  });
+
+  if (productosMostrados && productosMostrados.length > 0) {
+    productosMostrados.forEach(function(producto, index) {
+      const li = document.createElement('li');
+      li.innerHTML = `Producto: ${producto.nombre}, Valor en pesos: ${producto.valorEnPesos}, Precio en dólares: $${producto.precioEnDolares.toFixed(2)} 
+      <button class="borrar" data-index="${index}">Borrar</button>`;
+      listaProductos.appendChild(li);
+    });
+  } else {
+    listaProductos.innerHTML = 'No se encontraron productos.';
+  }
 }
 
-agregarProductoBtn.addEventListener('click', agregarProducto);
-filtroInput.addEventListener('input', function() {
-  let filtroTexto = filtroInput.value.toLowerCase();
+function guardarProductosEnLocalStorage(productos) {
+  localStorage.setItem('productos', JSON.stringify(productos));
+}
 
-  let productosFiltrados = productos.filter(function(producto) {
-    return producto.nombre.toLowerCase().includes(filtroTexto);
-  });
-  mostrarProductos();
+function obtenerProductosDesdeLocalStorage() {
+  const productosJSON = localStorage.getItem('productos');
+  return productosJSON ? JSON.parse(productosJSON) : [];
+}
+
+function filtrarProductos() {
+  const filtroTexto = filtroInput.value.toLowerCase();
+  
+  if (filtroTexto.trim() === '') {
+    // Si el filtro está vacío o solo contiene espacios en blanco, mostrar todos los productos
+    mostrarProductos(productos);
+  } else {
+    const productosFiltrados = productos.filter(function(producto) {
+      return producto.nombre.toLowerCase().includes(filtroTexto);
+    });
+  
+    mostrarProductos(productosFiltrados);
+  }
+}
+
+listaProductos.addEventListener('click', function(e) {
+  if (e.target.classList.contains('borrar')) {
+    const index = e.target.getAttribute('data-index');
+    productos.splice(index, 1);
+    guardarProductosEnLocalStorage(productos);
+    mostrarProductos();
+  }
 });
 
 function actualizarFechaYHora() {
   const fechaActual = new Date();
   const fechaLegible = fechaActual.toLocaleDateString();
   const horaLegible = fechaActual.toLocaleTimeString();
-  const fechaYHora = `${fechaLegible} - ${horaLegible}`;
-  const elementoFechaHora = document.getElementById("fechaHora");
-  elementoFechaHora.textContent = "Fecha y hora actual: " + fechaYHora;
+  const fechaYHoraActual = `${fechaLegible} - ${horaLegible}`;
+  fechaHora.textContent = "Fecha y hora actual: " + fechaYHoraActual;
 }
+
 actualizarFechaYHora();
 setInterval(actualizarFechaYHora, 1000);
